ce9e03d5f2a0420edbefb2926ae050d8c208cd17
kimhyerim <kimhr4@visang.com>
2025-06-12 14:49:55 +0900

    Feat.

--- src/components/object_tool/ObjectTool.tsx
+++ src/components/object_tool/ObjectTool.tsx
@@ -5,9 +5,11 @@ import boldIcon from '../../assets/images/ooui_bold-b.svg';
 import * as fabric from 'fabric'; // v6
 
 function ObjectTool ({canvas, objectTool}: {canvas: fabric.Canvas, objectTool: { type: string; x: number; y: number }}) {
-    
     return (
         <div className={classes.tool_wrap} style={{ left: objectTool.x, top: objectTool.y}}>
+            <button title="색상" className={classes.tool_button}>
+                <span className={classes.palette} style={{backgroundColor: '#ff0000'}} />
+            </button>
             {objectTool.type==='textbox' && 
                 <>
                     <button className={classes.tool_button} title="글꼴 변경하기">
